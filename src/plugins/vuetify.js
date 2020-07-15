bd1b5078b7d622c6280d401eec26d9b299cecd08
yudartcode <yuda.karnaen11@gmail.com>
2020-07-15 11:57:29 +0800

    add theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,4 +7,14 @@ export default new Vuetify({
   icons: {
     iconfont: 'mdi',
   },
+  theme: {
+    themes: {
+      light: {
+        primary: '#002d40',
+        secondary: '#1aa6b7',
+        accent: '#ff414d',
+        error: '#f56a79',
+      },
+    },
+  },
 });
