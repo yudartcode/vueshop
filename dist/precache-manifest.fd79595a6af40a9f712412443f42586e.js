111d952e695a5f6b5465d02708b05e66a478e72b
yudartcode <yuda.karnaen11@gmail.com>
2020-07-17 17:26:20 +0800

    add dist file

---
+++ dist/precache-manifest.fd79595a6af40a9f712412443f42586e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "60dff12ccf3484b10ca1",
+    "url": "/js/about.facc344d.js"
+  },
+  {
+    "revision": "fd87c2aad13051609a8c",
+    "url": "/css/alert.9714bc3c.css"
+  },
+  {
+    "revision": "fd87c2aad13051609a8c",
+    "url": "/js/alert.7f79be24.js"
+  },
+  {
+    "revision": "ada272ab04612fe7098a",
+    "url": "/js/app.6d60775f.js"
+  },
+  {
+    "revision": "d32203027c0add4ed856",
+    "url": "/css/book.5aec967f.css"
+  },
+  {
+    "revision": "d32203027c0add4ed856",
+    "url": "/js/book.6fb1a042.js"
+  },
+  {
+    "revision": "7f9f787222bc5b5dc3db",
+    "url": "/js/book-item.bc2a56fe.js"
+  },
+  {
+    "revision": "6b139fe8dc5495bccaad",
+    "url": "/css/books.13e27461.css"
+  },
+  {
+    "revision": "6b139fe8dc5495bccaad",
+    "url": "/js/books.bbf8d029.js"
+  },
+  {
+    "revision": "eb3e78fdcd26c171c666",
+    "url": "/css/cart.536ad13c.css"
+  },
+  {
+    "revision": "eb3e78fdcd26c171c666",
+    "url": "/js/cart.396f6933.js"
+  },
+  {
+    "revision": "283062f594a20319e431",
+    "url": "/css/categories.13e27461.css"
+  },
+  {
+    "revision": "283062f594a20319e431",
+    "url": "/js/categories.ec178a73.js"
+  },
+  {
+    "revision": "f5bd7bc11873344b59ff",
+    "url": "/css/category.13e27461.css"
+  },
+  {
+    "revision": "f5bd7bc11873344b59ff",
+    "url": "/js/category.d636f411.js"
+  },
+  {
+    "revision": "277fe76c364b3ec30ff6",
+    "url": "/css/checkout.f787d260.css"
+  },
+  {
+    "revision": "277fe76c364b3ec30ff6",
+    "url": "/js/checkout.231821e3.js"
+  },
+  {
+    "revision": "3a0cbee90c55b5c9f194",
+    "url": "/css/chunk-vendors.cd031d5f.css"
+  },
+  {
+    "revision": "3a0cbee90c55b5c9f194",
+    "url": "/js/chunk-vendors.3dbb2661.js"
+  },
+  {
+    "revision": "e3d0e4e937100a436f1d",
+    "url": "/js/login.4b057cf0.js"
+  },
+  {
+    "revision": "9e42c766a390550367bd",
+    "url": "/css/my-order.6f33d223.css"
+  },
+  {
+    "revision": "9e42c766a390550367bd",
+    "url": "/js/my-order.632529e3.js"
+  },
+  {
+    "revision": "0713b8b8e9ff4cdebf0b",
+    "url": "/css/payment.6f33d223.css"
+  },
+  {
+    "revision": "0713b8b8e9ff4cdebf0b",
+    "url": "/js/payment.dffdbd94.js"
+  },
+  {
+    "revision": "6f639f76033197cf7ffa",
+    "url": "/css/profile.6f33d223.css"
+  },
+  {
+    "revision": "6f639f76033197cf7ffa",
+    "url": "/js/profile.184fc0e0.js"
+  },
+  {
+    "revision": "24b75148ae70361ec6f0",
+    "url": "/css/register.21fe44c6.css"
+  },
+  {
+    "revision": "24b75148ae70361ec6f0",
+    "url": "/js/register.c288c9d5.js"
+  },
+  {
+    "revision": "3da78002c7169c989fdf",
+    "url": "/css/search.536ad13c.css"
+  },
+  {
+    "revision": "3da78002c7169c989fdf",
+    "url": "/js/search.60c666b4.js"
+  },
+  {
+    "revision": "cbf730722e0b317fde372c13e5223e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88b6672388a68dc84649688c8628625d",
+    "url": "/img/bca.png"
+  },
+  {
+    "revision": "002a96bf1caef8c8142440b8a9348e5a",
+    "url": "/img/mandiri.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1187928a3a4b7af2b3c44db6cb09b07f",
+    "url": "/img/unavailable.png"
+  }
+];
